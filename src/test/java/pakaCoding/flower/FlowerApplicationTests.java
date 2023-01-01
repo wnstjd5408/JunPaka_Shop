97ea4b19844ef074b2ac1cc291f1b0c0c8be369a
paka_coding <wnstjd5408@naver.com>
2023-01-01 22:01:26 +0900

    feat : 첫 main 커밋

---
+++ src/test/java/pakaCoding/flower/FlowerApplicationTests.java
@@ -0,0 +1,13 @@
+package pakaCoding.flower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
