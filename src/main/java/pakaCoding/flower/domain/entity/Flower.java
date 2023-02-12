e19c86076ddf00d96bad934604e83377ad68c44e
paka_coding <wnstjd5408@naver.com>
2023-02-13 06:10:23 +0900

    feat : Flower 수정, FlowerDto 추가

--- src/main/java/pakaCoding/flower/domain/entity/Flower.java
+++ src/main/java/pakaCoding/flower/domain/entity/Flower.java
@@ -3,34 +3,55 @@ package pakaCoding.flower.domain.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import pakaCoding.flower.dto.FlowerDto;
 
 @Entity
 @Getter @Setter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 @Builder
 public class Flower extends TimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name ="flower_id")
-    private Long id;
+    private Long id;            //번호
 
     @Column(name = "flower_name")
-    private String name;
+    private String name;        //이름
 
-    private int price;
-    private int stockQuantity;
+    private int price;          //가격
+    private int stockQuantity;  //수량
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name ="type_id")
     private Type type;
 
-    @ColumnDefault("0")
-    private Integer hitCount;
+    @ColumnDefault("0L")
+    private Long hitCount;      //조회수
+
+
+    private String delYn;       //삭제여부
+
+
+    public Flower delete(String delYn){
+        this.delYn = delYn;
+        return this;
+    }
 
 
 
+    @Builder
+    public Flower(FlowerDto flowerDto, Type type) {
+        this.name = flowerDto.getName();
+        this.price = flowerDto.getPrice();
+        this.stockQuantity = flowerDto.getStockQuantity();
+        this.type = type;
+        this.hitCount = 0L;
+        this.delYn = "N";
+    }
 }
 
 
---
+++ src/main/java/pakaCoding/flower/dto/FlowerDto.java
@@ -0,0 +1,57 @@
+package pakaCoding.flower.dto;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.web.multipart.MultipartFile;
+import pakaCoding.flower.domain.entity.Flower;
+import pakaCoding.flower.domain.entity.TimeEntity;
+import pakaCoding.flower.domain.entity.Type;
+import pakaCoding.flower.domain.entity.UploadFile;
+
+import java.io.File;
+import java.util.List;
+
+@Data
+public class FlowerDto extends TimeEntity {
+
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+    private Type type;
+    private Long hitCount;
+    private UploadFile uploadFile;
+    private List<MultipartFile> multipartFile;
+
+    public FlowerDto(String name, int price, int stockQuantity) {
+        this.name = name;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+
+    @Builder
+    public FlowerDto(Long id, String name, int price, int stockQuantity, Type type, Long hitCount, UploadFile uploadFile, List<MultipartFile> multipartFile) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+        this.type = type;
+        this.hitCount = hitCount;
+        this.uploadFile = uploadFile;
+        this.multipartFile = multipartFile;
+    }
+
+
+
+    public Flower toEntity(){
+        return Flower.builder()
+                .type(type)
+                .name(name)
+                .price(price)
+                .stockQuantity(stockQuantity)
+                .build();
+    }
+}
