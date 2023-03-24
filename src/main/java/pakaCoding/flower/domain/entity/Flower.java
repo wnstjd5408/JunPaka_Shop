56292ada08cd9be2a1f7abb69b06f800d1487815
paka_coding <wnstjd5408@naver.com>
2023-03-25 03:26:37 +0900

    feat : 상품 수량 추가, 제거 기능 OutOfStockException 오류 추가

--- src/main/java/pakaCoding/flower/domain/entity/Flower.java
+++ src/main/java/pakaCoding/flower/domain/entity/Flower.java
@@ -5,6 +5,7 @@ import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import pakaCoding.flower.domain.constant.FlowerSellStatus;
 import pakaCoding.flower.dto.FlowerFormDto;
+import pakaCoding.flower.exception.OutOfStockException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -74,6 +75,20 @@ public class Flower extends TimeEntity {
         this.flowerSellStatus =flowerDto.getFlowerSellStatus();
         this.hitCount = 0L;
         this.delYn = "N";
+    }
+
+    public void removeStockQuantity(int stock){
+
+        int restStock = this.stockQuantity - stock;
+        if(restStock <0){
+            throw new OutOfStockException("상품의 재고가 부족합니다. (현재 재고 수량 : " + this.stockQuantity + ")");
+
+        }
+        this.stockQuantity = restStock;
+    }
+
+    public void addStockQuantity(int stock){
+        this.stockQuantity += stock;
     }
 }
 
