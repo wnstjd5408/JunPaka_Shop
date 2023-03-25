c271a87adca38cfc7525120f3e5da80c1322becb
paka_coding <wnstjd5408@naver.com>
2023-03-26 01:03:04 +0900

    feat : 주문 기능 만듬

---
+++ src/main/java/pakaCoding/flower/dto/OrderDto.java
@@ -0,0 +1,18 @@
+package pakaCoding.flower.dto;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class OrderDto {
+
+    @NotNull(message = "상품 아이디는 필수 입력 값 입니다.")
+    private Long flowerId;
+
+
+    @Min(value=1, message = "최소 주문 수량은 1개 입니다.")
+    @Max(value=999, message = "최대 주문 수량은 999개 입니다.")
+    private int count;
+}
