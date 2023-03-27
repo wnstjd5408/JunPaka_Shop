f3079b941eb7cf9a80b1c1131727ede2a13e358b
paka_coding <wnstjd5408@naver.com>
2023-03-27 14:55:29 +0900

    feat : ajax 사용해서 주문기능 만듬

---
+++ src/main/java/pakaCoding/flower/controller/OrderController.java
@@ -0,0 +1,66 @@
+package pakaCoding.flower.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+import pakaCoding.flower.domain.entity.Type;
+import pakaCoding.flower.dto.MemberSessionDto;
+import pakaCoding.flower.dto.OrderDto;
+import pakaCoding.flower.service.OrderService;
+import pakaCoding.flower.service.TypeService;
+
+import java.security.Principal;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final TypeService typeService;
+    private final OrderService orderService;
+
+
+    @GetMapping(value = "/order")
+    public String orderPage(@SessionAttribute(name = "member", required = false) MemberSessionDto member,
+                            Model model) {
+
+        List<Type> types = typeService.allType();
+        model.addAttribute("types", types);
+        model.addAttribute("member", member);
+
+
+        return "forms/orderForm";
+    }
+
+    //단일 상품 주문
+    @PostMapping(value = "/order")
+    @ResponseBody
+    public ResponseEntity order(@RequestBody @Valid OrderDto orderDto,
+                                BindingResult bindingResult, Principal principal) {
+
+        if (bindingResult.hasErrors()) {
+            StringBuilder sb = new StringBuilder();
+            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+            for (FieldError fieldError : fieldErrors) {
+                sb.append(fieldError.getDefaultMessage());
+            }
+            return new ResponseEntity<String>(sb.toString(), HttpStatus.BAD_REQUEST);
+        }
+
+        Long orderId;
+
+        try{
+            orderId = orderService.order(orderDto, principal.getName());
+
+        }catch (Exception e){
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<Long>(orderId, HttpStatus.OK);
+    }
+}
