b5f935dc95123629dad2c16a3e5c87c796c802c2
paka_coding <wnstjd5408@naver.com>
2023-03-29 01:30:29 +0900

    feat : 로그인 valid, 상품등록 valid, 회원가입 valid 조건 해결

--- src/main/java/pakaCoding/flower/dto/MemberDto.java
+++ src/main/java/pakaCoding/flower/dto/MemberDto.java
@@ -1,6 +1,11 @@
 package pakaCoding.flower.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
 import pakaCoding.flower.domain.constant.Gender;
 import pakaCoding.flower.domain.constant.Role;
 import pakaCoding.flower.domain.entity.Address;
@@ -14,11 +19,20 @@ import java.time.LocalDate;
 @Builder
 public class MemberDto {
 
+    @NotBlank(message = "아이디를 입력해주세요")
     private String userid;
+
+    @Length(min=5, message = "최소 5글자 이상 입력해주세요")
+    @NotEmpty(message = "*비밀번호를 다시 입력해주세요")
     private String password;
+
+    @NotBlank(message = "이름을 입력해주세요")
     private String username;
     private Gender gender;
     private LocalDate birthDate;
+
+    @Email(message = "이메일 형식으로 입력해주세요")
+    @NotEmpty(message = "이메일을 입력해주세요")
     private String email;
     private String zipcode;
     private String streetAdr;
