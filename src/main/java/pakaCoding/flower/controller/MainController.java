181f9212859d00b7c768d0b2c2ae872840caec0c
paka_coding <wnstjd5408@naver.com>
2023-04-09 20:51:23 +0900

    update : FileImageService 수정

--- src/main/java/pakaCoding/flower/controller/MainController.java
+++ src/main/java/pakaCoding/flower/controller/MainController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import pakaCoding.flower.domain.entity.FileImage;
 import pakaCoding.flower.repository.FileImageRepository;
+import pakaCoding.flower.service.FileImageService;
 
 import java.io.File;
 import java.net.MalformedURLException;
@@ -21,7 +22,7 @@ import java.nio.file.Paths;
 @RequiredArgsConstructor
 public class MainController {
 
-    private final FileImageRepository fileImageRepository;
+    private final FileImageService fileService;
 
 
     //이미지 보여주기
@@ -29,13 +30,13 @@ public class MainController {
     @GetMapping("/display/fileImage={saveFileName}")
     public Resource downloadImage(@PathVariable String saveFileName) throws MalformedURLException {
 
-        FileImage fileImage = fileImageRepository.findBySavedFileImgName(saveFileName).get();
+        FileImage fileImage = fileService.findBySavedFileImgName(saveFileName);
 
         log.info("fileImage = {}", fileImage.getSavedFileImgName());
         Path saveFilePath = Paths.get(fileImage.getUploadDir() + File.separator + fileImage.getSavedFileImgName());
 
         if(!saveFilePath.toFile().exists()){
-            throw new RuntimeException("File not Fount");
+            throw new RuntimeException("File not Found");
         }
         String fullPath = fileImage.getUploadDir() + fileImage.getSavedFileImgName();
         return new UrlResource("file:" + fullPath);
