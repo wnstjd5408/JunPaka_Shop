fb76359dc74a152b56706ae40735864100d6a218
paka_coding <wnstjd5408@naver.com>
2023-04-26 02:07:41 +0900

    refactor: 장바구니 주문 하는거 리펙토링

--- src/main/java/pakaCoding/flower/service/OrderService.java
+++ src/main/java/pakaCoding/flower/service/OrderService.java
@@ -71,9 +71,8 @@ public class OrderService {
             OrderItem orderItem = OrderItem.createOrderItem(flower, flower.getPrice(), orderDto.getCount());
             orderItemList.add(orderItem);
         }
-        Delivery delivery = new Delivery();
-        delivery.setAddress(member.getAddress());
-        delivery.setDeliveryStatus(DeliveryStatus.READY);
+        //Delivery 객체 생성
+        Delivery delivery = Delivery.createDelivery(member);
         //Order Entity 클래스에 존재하는 createOrder 메소드로 Order 생성 및 저장
         Order order = Order.createOrder(member, delivery, orderItemList);
         orderRepository.save(order);
