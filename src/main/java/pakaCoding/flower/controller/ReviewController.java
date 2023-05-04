549013b042613b11e2462d19993ece2d1aa37643
paka_coding <wnstjd5408@naver.com>
2023-05-05 02:47:32 +0900

    feat : 리뷰 Get 방식으로 전송

---
+++ src/main/java/pakaCoding/flower/controller/ReviewController.java
@@ -0,0 +1,51 @@
+package pakaCoding.flower.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import pakaCoding.flower.domain.entity.Type;
+import pakaCoding.flower.dto.ReviewFormDto;
+import pakaCoding.flower.service.CartService;
+import pakaCoding.flower.service.TypeService;
+
+import java.security.Principal;
+import java.util.List;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class ReviewController {
+
+    private final TypeService typeService;
+    private final CartService cartService;
+
+    @GetMapping("/reviews/form")
+    public String review(Principal principal, @RequestParam long orderItemNo, Model model){
+        if(principal != null){
+            model.addAttribute("member", principal.getName());
+            addCartCount(cartService.getCartListCount(principal.getName()), model);
+        }
+        else{
+            model.addAttribute("cartCount", 0);
+        }
+        List<Type> types = typeService.allType();
+
+        model.addAttribute("orderItemNo", orderItemNo);
+        model.addAttribute("types", types);
+        model.addAttribute("reviewFormDto", new ReviewFormDto());
+
+        return "reviews/reviewForm";
+    }
+
+    //CartCount 추가
+    private void addCartCount(Integer cartService, Model model) {
+        Integer count = cartService;
+        log.info("카트의 수 = {} ", count);
+        model.addAttribute("cartCount", count);
+    }
+
+}
---
+++ src/main/java/pakaCoding/flower/dto/ReviewFormDto.java
@@ -0,0 +1,14 @@
+package pakaCoding.flower.dto;
+
+import lombok.Data;
+
+@Data
+public class ReviewFormDto {
+
+    private String comment;
+
+    private Integer rating;
+
+
+
+}
